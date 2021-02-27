b4267649dd05ab93d9aade8db5aa83065dd0972a
ConxiCr <conxicr@hotmail.com>
2021-02-27 18:23:52 +0100

    create component formulary

---
+++ src/app/components/formulary/formulary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulary',
+  templateUrl: './formulary.component.html',
+  styleUrls: ['./formulary.component.css']
+})
+export class FormularyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
